dd9b90e3ecba9b419e93e6e9378515052df4d541
zeib2269 <zeib2269@colorado.edu>
2025-10-07 13:43:54 -0600

    Add files via upload

--- LAB3PROB32CODE.m
+++ LAB3PROB32CODE.m
@@ -23,7 +23,7 @@ for i = 1:length(trials)
 
     trials_data{i} = readmatrix(trials(i));
     time{i} = (trials_data{i}(:,1))/(10^3); %conversion from ms to sec
-    torque{i} = (trials_data{i}(:,4))*(33.5); %multiply by torque constant
+    torque{i} = (trials_data{i}(:,4))*(33.5); %multiply by torque constant mNm
     angular_velocity{i} = (trials_data{i}(:,3))* ((2*pi)/60); %Convert from RPM to rad/s
 
      if i <= 3
@@ -33,11 +33,27 @@ for i = 1:length(trials)
     else
         t_min = 1; t_max = 4;
     end
-
+    
+    %Polyfit for alpha
     linear_idx = (time{i} >= t_min & time{i} <= t_max);
     p = polyfit(time{i}(linear_idx), angular_velocity{i}(linear_idx), 1);
     w_fit = polyval(p, time{i}(linear_idx));
-   
+
+    %Polyfit for Torque
+    linear_idx_tor = (time{i} >= t_min & time{i} <= t_max);
+    avg_torque = mean(torque{i}(linear_idx_tor));
+
+    figure()
+    plot(time{i}, torque{i})
+    hold on;
+    yline(avg_torque,'r', 'LineWidth', 2)
+    title(['Torque for Trial ' num2str(i) ': ' num2str(torques(i)) ' mNm']);
+    xlabel('Time (s)');
+    ylabel('Torque (mNm)');
+    legend('Measured Torque', 'Average Torque', 'Location', 'best');
+    grid on;
+    hold off; % Release the hold on the current figure
+
     figure()
     plot(time{i}, angular_velocity{i}, 'b');
     hold on;
@@ -46,16 +62,20 @@ for i = 1:length(trials)
     xlabel('Time (s)');
     ylabel('Angular Velocity (rad/s)');
     legend('Measured Data', 'Linear Fit', 'Location', 'best');
+    grid on;
+
+    avg_torque(i) = avg_torque;
 
     alpha(i) = p(1);
+    moi(i) = (avg_torque(i)*(0.001))./ alpha(i); % Using average torque and angular acceleration
+    fprintf('Trial %d: Torque = %.3f mNm | Alpha = %.3f rad/s^2 | I = %.4e kg*m^2\n', ...
+        i, avg_torque(i), alpha(i), moi(i));
 end
-
-moi = torques./alpha;
 
 stdev = std(moi);
 mean_moi = mean(moi); 
-fprintf('Standard Deviation of Moment of Inertia: %.2f kg*m^2\n', stdev);
-fprintf('Mean Moment of Inertia: %.2f kg*m^2\n', mean_moi);
+fprintf('Standard Deviation of Moment of Inertia: %.4e kg*m^2\n', stdev);
+fprintf('Mean Moment of Inertia: %.4e kg*m^2\n', mean_moi);
 
 % Tau = I* alpha
 tau_aero = 1e-4; % Nm
@@ -67,7 +87,7 @@ alpha_aero = tau_aero / mean_moi; % rad/s^2
 % omega = alpha * time
 time_to_limit = omega_limit / alpha_aero; % seconds
 
-H = mean_moi * omega_limit; % N·m·s
+H = mean_moi * omega_limit; % N·m·s H = I* omega
 
 fprintf('\nAngular acceleration due to 1e-4 Nm torque: %.4e rad/s^2\n', alpha_aero);
 fprintf('Time to reach 4000 rpm: %.2f seconds\n', time_to_limit);
